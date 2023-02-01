9131f0bcc3ddd13c22f48bcee45f585909a602e2
Fabian Buitrago <fabianbuitragov@gmail.com>
2023-02-01 13:43:38 -0500

    Review hooks

--- components/Cat.tsx
+++ components/Cat.tsx
@@ -1,14 +1,25 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export type Props = {
   name: string;
 };
 
 const Cat = (props: Props) => {
+  const [isHungry, setIsHungry] = useState(true);
+
   return (
     <View>
-      <Text>Hello, I am {props.name}!</Text>
+      <Text>
+        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+      />
     </View>
   );
 };
